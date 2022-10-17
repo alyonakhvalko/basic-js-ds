fbebecad8780a275c3be6e661d904fba6af994c5
Alyona Khvalko <85245607+alyonakhvalko@users.noreply.github.com>
2022-10-17 09:43:36 +0300

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -30,6 +30,32 @@ const { NotImplementedError } = require('../extensions/index.js');
   }
 }
 
+/*function Stack() {
+    this.count = 0
+    this.storage = {}
+
+    this.push = function(value) {
+        this.storage[this.count] = value
+        this.count++
+    }
+
+    this.pop = function() {
+        if (this.count === 0) return undefined
+        this.count--
+        let result = this.storage[this.count]
+        delete this.storage[this.count]
+        return result
+    }
+
+    this.peek = function() {
+        return this.storage[this.count - 1]
+    }
+
+    this.size = function() {
+        return this.count
+    }
+}*/
+
 module.exports = {
   Stack
 };
